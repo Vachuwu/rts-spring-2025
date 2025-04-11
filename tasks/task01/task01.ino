8845803a5be9786db75896b181a6183e57530dbf
Vachuwu <absolem47@mail.ru>
2025-04-11 23:46:25 +0400

    add solutions

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,68 @@
-void setup() {
+const int pin10ms = 3;
+const int pin1ms = 5;
+const int pin500us = 6;
+const int pin100us = 9;
+const int pin50us = 10;
 
+int state10ms = LOW;
+int state1ms = LOW;
+int state500us = LOW;
+int state100us = LOW;
+int state50us = LOW;
+
+unsigned long previousMillis10ms = 0;
+unsigned long previousMillis1ms = 0;
+unsigned long previousMicros500us = 0;
+unsigned long previousMicros100us = 0;
+unsigned long previousMicros50us = 0;
+
+const long interval10ms = 10;        // период 10 мс 
+const long interval1ms = 1;          // период 1 мс 
+const long interval500us = 500;      // период 500 мкс 
+const long interval100us = 100;      // период 100 мкс 
+const long interval50us = 50;        // период 50 мкс 
+
+void setup() {
+  pinMode(pin10ms, OUTPUT);
+  pinMode(pin1ms, OUTPUT);
+  pinMode(pin500us, OUTPUT);
+  pinMode(pin100us, OUTPUT);
+  pinMode(pin50us, OUTPUT);
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+  unsigned long currentMicros = micros();
 
+  if (currentMillis - previousMillis10ms >= interval10ms) {
+    previousMillis10ms = currentMillis;
+    state10ms = !state10ms;
+    digitalWrite(pin10ms, state10ms);
+  }
+
+
+  if (currentMillis - previousMillis1ms >= interval1ms) {
+    previousMillis1ms = currentMillis;
+    state1ms = !state1ms;
+    digitalWrite(pin1ms, state1ms);
+  }
+
+  if (currentMicros - previousMicros500us >= interval500us) {
+    previousMicros500us = currentMicros;
+    state500us = !state500us;
+    digitalWrite(pin500us, state500us);
+  }
+
+  // Генерация 100 мкс импульса (50 мкс HIGH, 50 мкс LOW)
+  if (currentMicros - previousMicros100us >= interval100us) {
+    previousMicros100us = currentMicros;
+    state100us = !state100us;
+    digitalWrite(pin100us, state100us);
+  }
+
+  if (currentMicros - previousMicros50us >= interval50us) {
+    previousMicros50us = currentMicros;
+    state50us = !state50us;
+    digitalWrite(pin50us, state50us);
+  }
 }
--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,58 @@
-void setup() {
+const int INTERRUPT_SIGNAL_PIN = 18;
+const int MEASUREMENT_BUFFER_SIZE = 1000;
 
+volatile uint32_t interruptCounter = 0;
+volatile uint32_t cumulativeTimeSum = 0;
+uint32_t timeDeltaBuffer[MEASUREMENT_BUFFER_SIZE];
+uint32_t lastInterruptTime = 0;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(INTERRUPT_SIGNAL_PIN, INPUT);
+    attachInterrupt(
+        digitalPinToInterrupt(INTERRUPT_SIGNAL_PIN), 
+        handleInterrupt, 
+        HIGH
+    );
 }
 
 void loop() {
+    if (interruptCounter >= MEASUREMENT_BUFFER_SIZE) {
+        noInterrupts();
+        computeAndPrintStatistics();
+        interruptCounter = 0;
+        cumulativeTimeSum = 0;
+        interrupts();
+    }
+}
 
+void handleInterrupt() {
+    uint32_t currentTime = micros();
+    uint32_t timeSinceLastInterrupt = currentTime - lastInterruptTime;
+    
+    if (interruptCounter < MEASUREMENT_BUFFER_SIZE) {
+        timeDeltaBuffer[interruptCounter] = timeSinceLastInterrupt;
+        cumulativeTimeSum += timeSinceLastInterrupt;
+        interruptCounter++;
+    }
+    
+    lastInterruptTime = currentTime;
+}
+
+void computeAndPrintStatistics() {
+    float meanInterval = cumulativeTimeSum / (float)MEASUREMENT_BUFFER_SIZE;
+    float sumSquaredDeviations = 0;
+    
+    for (int i = 0; i < MEASUREMENT_BUFFER_SIZE; i++) {
+        float deviation = timeDeltaBuffer[i] - meanInterval;
+        sumSquaredDeviations += deviation * deviation;
+    }
+    
+    float rootMeanSquareDeviation = sqrt(sumSquaredDeviations / MEASUREMENT_BUFFER_SIZE);
+    
+    Serial.print("Mean interval: ");
+    Serial.print(meanInterval);
+    Serial.print(" us, RMS deviation: ");
+    Serial.print(rootMeanSquareDeviation);
+    Serial.println(" us");
 }
